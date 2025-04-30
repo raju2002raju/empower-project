06a0b0f89014fb579a8b57dee872933a14dcdd93
raju2002raju <151726988+raju2002raju@users.noreply.github.com>
2025-04-30 14:59:17 +0530

    Add files via upload

---
+++ src/components/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import DrawingCanvas from './components/DrawingCanvas'
+
+function App() {
+  return (
+    <div className="App">
+      <DrawingCanvas />
+    </div>
+  );
+}
+
+export default App;
